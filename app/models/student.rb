2f46faba14c0922134c571fe9e379351298dfecc
luke <lukekennedy212@gmail.com>
2018-12-27 13:53:48 +0800

    added the many to many

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,7 +4,7 @@ class Student < ActiveRecord::Base
     validates :email, uniqueness: true 
     validates :age, numericality: { greater_than: 5}
     validates :phone, length: { minimum: 10}
-    belongs_to :teacher
+    has_many :teacher, through: :student_teachers
     def name 
        "#{self.first_name} #{self.last_name}"
     end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,6 +2,6 @@ class Teacher < ActiveRecord::Base
     validates :email, format: {with: /.+@.+\...+/}
     validates :phone, length: { minimum: 10}
     validates :email, uniqueness: true 
-    has_many :students
+    has_many :students, through: :student_teachers
 end
 
